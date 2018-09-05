8ed0ed3a3806d87c966abe5b21a15c61a2e79bd3
JESSIE ANDERSON <jessanderson222@gmail.com>
2018-09-05 00:59:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,6 +38,5 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   }
   
   function removeElementFromEndOfArray(array) {
-    array.slice(0, array.length - 1)
-    return array
+   return array.slice(0, array.length - 1)
   }
